f6fb937bf629d014b5caeadef43fd498041ad53d
Kawsara Mohamed MK <mkwsra@gmail.com>
2020-05-08 02:37:11 +0300

    added new functions to get all types of ratings

--- src/Traits/CanBeRated.php
+++ src/Traits/CanBeRated.php
@@ -34,17 +34,17 @@ trait CanBeRated
         }
 
         /*
-         * todo: Check if the passed type exists in types array
-        *  config('acquaintances.rating.types')
-        */
+         * Todo: [minor feature] Check if the passed type exists in types array
+         *  config('acquaintances.rating.types')
+         */
 
         return self::$ratedType;
     }
 
     /**
-     * Check if user is isRatedBy by given user.
+     * Check if a model is isRatedBy by given user.
      *
-     * @param  int  $user
+     * @param  int|array|\Illuminate\Database\Eloquent\Model  $user
      *
      * @return bool
      */
@@ -64,9 +64,7 @@ trait CanBeRated
             config('acquaintances.tables.interactions'))
                          ->wherePivot('relation', '=', Interaction::RELATION_RATE);
 
-        if ($this->ratedType() != 'overall') {
-            $relation = $relation->wherePivot('relation_type', '=', $this->ratedType());
-        }
+        $relation = $relation->wherePivot('relation_type', '=', $this->ratedType());
 
         return $relation->withPivot(...Interaction::$pivotColumns);
     }
@@ -78,10 +76,20 @@ trait CanBeRated
         return $this->raters()->avg('relation_value');
     }
 
+    public function averageRatingAllTypes($ratingType = null)
+    {
+        return $this->raters()->avg('relation_value');
+    }
+
     public function sumRating($ratingType = null)
     {
         $this->ratedType($ratingType);
 
+        return $this->raters()->sum('relation_value');
+    }
+
+    public function sumRatingAllTypes()
+    {
         return $this->raters()->sum('relation_value');
     }
 
@@ -90,10 +98,20 @@ trait CanBeRated
         return Interaction::numberToReadable($this->sumRating($ratingType), $precision, $divisors);
     }
 
+    public function sumRatingAllTypesReadable($precision = 1, $divisors = null)
+    {
+        return Interaction::numberToReadable($this->sumRatingAllTypes(), $precision, $divisors);
+    }
+
     public function userAverageRating($ratingType = null)
     {
         $this->ratedType($ratingType);
 
+        return $this->raters()->where('user_id', \Auth::id())->avg('relation_value');
+    }
+
+    public function userAverageRatingAllTypes()
+    {
         return $this->raters()->where('user_id', \Auth::id())->avg('relation_value');
     }
 
@@ -104,9 +122,20 @@ trait CanBeRated
         return $this->raters()->where('user_id', \Auth::id())->sum('relation_value');
     }
 
+    public function userSumRatingAllTypes()
+    {
+        return $this->raters()->where('user_id', \Auth::id())->sum('relation_value');
+    }
+
     public function userSumRatingReadable($ratingType = null, $precision = 1, $divisors = null)
     {
-        return Interaction::numberToReadable($this->sumRating($ratingType), $precision, $divisors);
+        return Interaction::numberToReadable($this->userSumRating($ratingType), $precision, $divisors);
+    }
+
+
+    public function userSumRatingAllTypesReadable($precision = 1, $divisors = null)
+    {
+        return Interaction::numberToReadable($this->userSumRatingAllTypes(), $precision, $divisors);
     }
 
     /**
@@ -131,9 +160,35 @@ trait CanBeRated
         return ($quantity * $max) > 0 ? $total / (($quantity * $max) / 100) : 0;
     }
 
+    /**
+     * Calculating the percentage based on the passed coefficient
+     * Taking the default value of $max from within the config file
+     *
+     * @param  null  $max
+     *
+     * @param  null  $ratingType
+     *
+     * @return float|int
+     */
+    public function ratingPercentAllTypes($max = null)
+    {
+        if (empty($max)) {
+            $max = config('acquaintances.rating.defaults.amount');
+        }
+        $quantity = $this->raters()->count();
+        $total = $this->sumRating();
+
+        return ($quantity * $max) > 0 ? $total / (($quantity * $max) / 100) : 0;
+    }
+
     public function getAverageRatingAttribute()
     {
         return $this->averageRating();
+    }
+
+    public function getAverageRatingAllTypesAttribute()
+    {
+        return $this->averageRatingAllTypes();
     }
 
     public function getSumRatingAttribute()
@@ -141,14 +196,29 @@ trait CanBeRated
         return $this->sumRating();
     }
 
+    public function getSumRatingAllTypesAttribute()
+    {
+        return $this->sumRatingAllTypes();
+    }
+
     public function getUserAverageRatingAttribute()
     {
         return $this->userAverageRating();
     }
 
+    public function getUserAverageRatingAllTypesAttribute()
+    {
+        return $this->userAverageRatingAllTypes();
+    }
+
     public function getUserSumRatingAttribute()
     {
         return $this->userSumRating();
+    }
+
+    public function getUserSumRatingAllTypesAttribute()
+    {
+        return $this->userSumRatingAllTypes();
     }
 
 }
