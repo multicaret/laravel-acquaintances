655731f7ad73502ba979092b217bd93dacede697
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-03-13 20:53:11 +0100

    fix: use new helper method get user model name

--- src/Traits/CanBeFavorited.php
+++ src/Traits/CanBeFavorited.php
@@ -30,7 +30,7 @@ trait CanBeFavorited
      */
     public function favoriters()
     {
-        return $this->morphToMany(config('auth.providers.users.model'), 'subject',
+        return $this->morphToMany(Interaction::getUserModelName(), 'subject',
             config('acquaintances.tables.interactions'))
                     ->wherePivot('relation', '=', Interaction::RELATION_FAVORITE)
                     ->withPivot(...Interaction::$pivotColumns);
