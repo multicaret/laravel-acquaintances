0f32fe1fd1e10952372b7a6f629d36f5297aa9c8
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-03-16 15:46:14 +0100

    fix: use new user model helper method

--- src/Traits/CanBeViewed.php
+++ src/Traits/CanBeViewed.php
@@ -29,7 +29,7 @@ trait CanBeViewed
      */
     public function viewers()
     {
-        return $this->morphToMany(config('auth.providers.users.model'), 'subject',
+        return $this->morphToMany(Interaction::getUserModelName(), 'subject',
             config('acquaintances.tables.interactions'))
                     ->wherePivot('relation', '=', Interaction::RELATION_VIEW)
                     ->withPivot(...Interaction::$pivotColumns);
