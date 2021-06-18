32d60576d82ccf981e64d86c1684d5f564b88d1f
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-06-18 14:08:21 +0200

    feat: allow user to turn off migrations

--- config/acquaintances.php
+++ config/acquaintances.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
+    /**
+     * Load migrations from package migrations,
+     * if You published the migration files, please set to `false`.
+     */
+    'migrations' => false,
+    
     /*
      * Models Related.
      */
